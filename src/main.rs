f39844cc79fbb7f4b4498f8483436d545612dfe6
marcello <marcello.filopanti@gmail.com>
2020-11-27 11:47:09 +0100

    add_layer() in Arena10x10 struct. show() prints the first non-empty tile of the layers stack in Arena struct.

--- src/main.rs
+++ src/main.rs
@@ -8,7 +8,11 @@ struct Actor {
 }
 impl Actor {
     fn new(x: i32, y: i32) -> Actor {
-        Actor { x, y, dir: Direction::Hold }
+        Actor {
+            x,
+            y,
+            dir: Direction::Hold,
+        }
     }
     fn redir(&mut self, direction: Direction) {
         self.dir = direction;
@@ -19,7 +23,7 @@ impl Actor {
             Direction::Right => self.x += 1,
             Direction::Down => self.y += 1,
             Direction::Left => self.x -= 1,
-            Direction::Hold => {},
+            Direction::Hold => {}
         }
     }
     fn think() -> Direction {
@@ -41,7 +45,7 @@ struct Tile {
 }
 impl Tile {
     fn new(img_char: char, walkable: bool) -> Tile {
-        Tile{img_char, walkable}
+        Tile { img_char, walkable }
     }
     fn new_empty() -> Tile {
         Tile::new(' ', true)
@@ -52,11 +56,13 @@ impl Tile {
 }
 
 struct Layer10x10 {
-    tiles: [ [Tile; 10] ; 10 ]
+    tiles: [[Tile; 10]; 10],
 }
 impl Layer10x10 {
     fn new_empty() -> Layer10x10 {
-        Layer10x10{tiles: [ [Tile::new_empty(); 10] ; 10 ]}
+        Layer10x10 {
+            tiles: [[Tile::new_empty(); 10]; 10],
+        }
     }
     fn new_border() -> Layer10x10 {
         let mut layer = Layer10x10::new_empty();
@@ -67,7 +73,6 @@ impl Layer10x10 {
             layer.put(Tile::new_block(), i, 9);
         }
         layer
-
     }
     fn put(&mut self, tile: Tile, x: u32, y: u32) {
         self.tiles[y as usize][x as usize] = tile;
@@ -78,16 +83,26 @@ impl Layer10x10 {
 }
 
 struct Arena10x10 {
-    layers: Vec<Layer10x10>
+    layers: Vec<Layer10x10>,
 }
 impl Arena10x10 {
     fn new() -> Arena10x10 {
-        Arena10x10{layers: Vec::new() }
+        Arena10x10 { layers: Vec::new() }
+    }
+    fn add_layer(&mut self, layer: Layer10x10) {
+        self.layers.push(layer);
     }
     fn show(self) {
         for y in 0..10 {
             for x in 0..10 {
-                print!("{}", self.layers[0].get(x, y).img_char );
+                let mut img_char = ' ';
+                for layer in &self.layers {
+                    img_char = layer.get(x, y).img_char;
+                    if img_char != ' ' {
+                        break;
+                    }
+                }
+                print!("{}", img_char);
             }
             println!();
         }
@@ -104,10 +119,15 @@ enum Direction {
 }
 
 fn main() {
-    let empty = Layer10x10::new_empty();
+    let mut pillars = Layer10x10::new_empty();
+    pillars.put(Tile::new_block(),4, 4);
+    pillars.put(Tile::new_block(),4, 5);
+    pillars.put(Tile::new_block(),5, 4);
+    pillars.put(Tile::new_block(),5, 5);
     let squared = Layer10x10::new_border();
     let mut arena = Arena10x10::new();
-    arena.layers.push(squared);
+    arena.add_layer(squared);
+    arena.add_layer(pillars);
     arena.show();
 
     // let mut act = Actor::new(5, 5);
